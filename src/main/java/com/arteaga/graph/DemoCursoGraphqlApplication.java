3f580400d3db647bfa5e4218c521a5b67f209c71
Alisson <alissondiane.arteagaquico@nttdata.com>
2023-07-06 15:39:46 -0500

    first configuration in project¿

---
+++ src/main/java/com/arteaga/graph/DemoCursoGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.arteaga.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCursoGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCursoGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arteaga/graph/DemoCursoGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arteaga.graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCursoGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
