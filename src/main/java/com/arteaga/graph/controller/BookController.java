4c6a1d0d62c0b6699cdb195852bad6f01173520e
Alisson <alissondiane.arteagaquico@nttdata.com>
2023-07-19 12:17:24 -0500

    prueba graphql

---
+++ src/main/java/com/arteaga/graph/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.arteaga.graph.controller;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import com.arteaga.graph.model.Author;
+import com.arteaga.graph.model.Book;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.getAuthorId());
+    }
+}
